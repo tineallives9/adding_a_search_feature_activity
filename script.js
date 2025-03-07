47785e3b7a97e717f24d0912143d7dea6b397f63
tineallives9 <sevilla.christine000@gmail.com>
2025-03-07 08:19:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("https://raw.githubusercontent.com/Jayrille/my-portfolio/refs/heads/main/courses.json")  
+        .then(response => response.json())
+        .then(data => {
+            const coursesTable = document.getElementById("courses-list");
+
+            data.courses.forEach(course => {
+                let row = document.createElement("tr");
+
+                row.innerHTML = `
+                    <td>${course.year_level}</td>
+                    <td>${course.sem}</td>
+                    <td>${course.code}</td>
+                    <td>${course.description}</td>
+                    <td>${course.credit}</td>
+                `;
+
+                coursesTable.appendChild(row);
+            });
+        })
+        .catch(error => console.error("Error loading courses:", error));
+});
